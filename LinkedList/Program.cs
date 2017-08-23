d7fce1322ed74fbb8aa8bef0e84634deec88f866
Allen <455300916@qq.com>
2017-08-23 21:42:55 +0800

    实现双链表新增节点删除节点

--- LinkedList/Program.cs
+++ LinkedList/Program.cs
@@ -10,11 +10,48 @@ namespace LinkedList
     {
         static void Main(string[] args)
         {
-            TestSingleLinkedList();
+            //TestSingleLinkedList();
+            TestDoubleLinkedList();
+
+            LinkedList<int> linkedList = new LinkedList<int>();
+            LinkedListNode<int> node1 = new LinkedListNode<int>(1);
+            //LinkedListNode<int> node2 = new LinkedListNode<int>(2);
+            //LinkedListNode<int> node3 = new LinkedListNode<int>(3);
+            linkedList.AddFirst(node1);
+            //linkedList.AddFirst(node2);
+            //linkedList.AddFirst(node3);
 
             Console.ReadLine();
         }
 
+        public static void TestDoubleLinkedList()
+        {
+            MyDoubleLinkedList<int> linkedList = new MyDoubleLinkedList<int>();
+
+            DbNode<int> node1 = new DbNode<int>(1);
+            DbNode<int> node2 = new DbNode<int>(2);
+            DbNode<int> node3 = new DbNode<int>(3);
+            linkedList.AddBefore(1);
+            linkedList.AddBefore(2);
+            linkedList.AddBefore(3);
+            linkedList.AddBefore(4);
+            linkedList.AddAfter(5);
+
+            for (int i = 0; i < linkedList.Count; i++)
+            {
+                Console.WriteLine(linkedList[i]);
+            }
+
+            Console.WriteLine("Remove at 1");
+            linkedList.RemoveAt(0);
+
+            for (int i = 0; i < linkedList.Count; i++)
+            {
+                Console.WriteLine(linkedList[i]);
+            }
+
+
+        }
 
         public static void TestSingleLinkedList()
         {
